4a13a30fe00abec834c4d03735bc858f2d0ebc82
PranavKonduru12 <pskonduru12+gh@gmail.com>
2026-05-14 14:53:11 -0700

    cleaner workspace with the connect4game, first commit

---
+++ Connect4_CortexM0/kiel_software_api/wells version edk_driver.d
@@ -0,0 +1,9 @@
+.\wells version edk_driver.o: ..\wells version edk_driver.c
+.\wells version edk_driver.o: .\Device\EDK_CM0.h
+.\wells version edk_driver.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\wells version edk_driver.o: .\Device\edk_driver.h
+.\wells version edk_driver.o: .\Core\core_cm0.h
+.\wells version edk_driver.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\wells version edk_driver.o: .\Core\core_cmInstr.h
+.\wells version edk_driver.o: .\Core\core_cmFunc.h
+.\wells version edk_driver.o: .\Device\edk_api.h
